5179141653cab12778058e10e36721c05464deed
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-20 13:41:05 +0200

    Cliente Pedro José Montellán

--- src/main.js
+++ src/main.js
@@ -1,33 +1,6 @@
-import "./style.css";
-
-console.log("Hola Mundo :D (otra vez)");
-
-console.log("2+2");
-
-console.log(2+2);
-
-console.log("Hola JavaScript");
-
-console.log(4*6);
-
-console.log(4-6);
-
-console.log(4/6);
-
-console.log(3**2);
-
-console.log((3+2)*8);
-
-console.log(53-17);
-
-console.log(3.14+1.1);
-
-console.log(Math.PI*4**2);
-
-console.log(Math.PI);
-
-Math.pow(4 , 2);
-
-console.log(true);
-
-console.log(true);
+console.log({
+    nifDNI: '12345678Z',
+    nombre: 'Pedro',
+    apellidos: 'José Montellán',
+    dirección: 'puente de los dulces'
+});
