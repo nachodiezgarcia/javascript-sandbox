108594e9e721608bc7571897bbc2059fabef46d5
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-20 13:30:19 +0200

    Decimales

--- src/main.js
+++ src/main.js
@@ -14,8 +14,17 @@ console.log(4-6);
 
 console.log(4/6);
 
-console.log(3**2)
+console.log(3**2);
 
-console.log((3+2)*8)
+console.log((3+2)*8);
 
-console.log(53-17)
+console.log(53-17);
+
+console.log(3.14+1.1);
+
+console.log(Math.PI*4**2);
+
+console.log(Math.PI);
+
+Math.pow(4 , 2);
+
