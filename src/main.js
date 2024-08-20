aa930b25f494fcd45aff4d14659c0585f7db0928
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-20 13:50:09 +0200

    Cliente con dirección mejorada

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 console.log({
-    nifDNI: '12345678Z',
-    nombre: 'Pedro',
-    apellidos: 'José Montellán',
-    dirección: 'puente de los dulces'
+    nifDNI: "12345678Z",
+    nombre: "Pedro",
+    apellidos: "José Montellán",
+    dirección:{
+        calle:"puente de los dulces",
+        ciudad:"París",
+        codicoPostal:"12239",
+    }
 });
