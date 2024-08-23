f75a6d186827ff1fbc93102fcac4d21c8ace06e5
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-23 13:50:21 +0200

    Todo con var

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-console.log({
-    DineroPorCadaComensal: console.log((120-18)/6)
-});
+var descuento = 0.9;
+
+var iva = 1.21;
+
+var precioProduct1 = 100;
+
+var precioProduct2 = 30;
+
+console.log(precioProduct1*descuento*iva+precioProduct2*descuento*iva);
+
+console.log("Mejor let que var");
