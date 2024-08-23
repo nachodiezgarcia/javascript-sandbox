e5096ce5668e03f1cf6404f98361cd4d522968f7
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-23 14:55:37 +0200

    Error de objeto Objeto

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
-const cliente = {
-    nombre:"Pedro" ,
-    apellido:"Perlosa Gabaño" ,
-    edad: 30 ,
-    direcición: {
-        calle:"calle eñe" ,
-        numero:5 ,
-        ciudad: "Fráncfort" ,
-        codigoPostal: 91234 
-    }
+let nombre = "Paco";
+
+let apellido = "Perez de Alaya";
+
+console.log(nombre + " " + apellido);
+
+nombre ={
+    Ciudad: "Madrid",
+    Calle: "Calle cadiz"
 };
 
-cliente.nombre = "Ana";
-
-console.log(cliente);
-
-let libro = {
-    titulo: "El guardian entre el centeno" ,
-    autor: "J.D Salinger",
-    añoDePublicación: 1000
-};
-
-libro.añoDePublicación = 1956;
-
-console.log(libro);
+console.log(nombre + " " + apellido);
