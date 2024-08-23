687fa71acb74a9f4bdf9bb69ada4dcb90b66140d
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-23 14:12:46 +0200

    Perlosa Gabaño

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-let descuento = 0.9;
+const cliente = {
+    nombre:"Pedro" ,
+    apellido:"Perlosa Gabaño" ,
+    edad: 30 ,
+    direcición: {
+        calle:"calle eñe" ,
+        numero:5 ,
+        ciudad: Fráncfort ,
+        codigoPostal: 91234 
+    }
+};
 
-let iva = 1.21;
-
-let precioProduct1 = 100;
-
-let precioProduct2 = 30;
-
-console.log(precioProduct1*descuento*iva+precioProduct2*descuento*iva);
-
-console.log("Mejor let que var");
-
-iva = 1.1; //Aplicamos un IVA menor
-
-/*Varias
-lineas :D*/
-
-console.log(precioProduct1*descuento*iva+precioProduct2*descuento*iva)
