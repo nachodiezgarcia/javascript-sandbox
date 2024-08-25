3e5735f2ace0fee5c1d78bac540390c350292778
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-25 14:27:21 +0200

    Todas las recetas y emojis

--- src/main.js
+++ src/main.js
@@ -1,27 +1,38 @@
 /*
-Ensalada de alubias con aguacate y huevo
-Ingredientes: alubias, aguacate, huevo, cebolla, tomate, lechuga.
-Preparación: cocer las alubias y dejar enfriar. Mezclar con aguacate en cubos, huevo cocido en rodajas, cebolla en juliana, tomate en cubos y lechuga troceada. Aliñar con aceite de oliva y vinagre.
-Ensalada de setas y bacon
-Ingredientes: setas, bacon, lechuga, tomate, cebolla, queso.
-Preparación: saltear las setas y el bacon hasta que estén dorados. Mezclar con lechuga troceada, tomate en cubos, cebolla en juliana y queso rallado. Aliñar con aceite de oliva y vinagre.
-Ensalada de arándanos y pollo
-Ingredientes: arándanos, pollo cocido, lechuga, zanahoria, queso, nueces.
-Preparación: mezclar los arándanos con el pollo cocido en cubos, lechuga troceada, zanahoria rallada, queso rallado y nueces picadas. Aliñar con una vinagreta de miel y mostaza.
-Ensalada de maíz y piña
-Ingredientes: maíz, piña en cubos, lechuga, pepino, cebolla, aguacate.
-Preparación: mezclar el maíz con la piña en cubos, lechuga troceada, pepino en rodajas, cebolla en juliana y aguacate en cubos. Aliñar con una vinagreta de aceite de oliva y vinagre.
-Ensalada de mango y queso feta
-Ingredientes: mango en cubos, queso feta, lechuga, cebolla, pepino, aceitunas.
-Preparación: mezclar el mango en cubos con el queso feta desmenuzado, lechuga troceada, cebolla en juliana, pepino en rodajas y aceitunas. Aliñar con una vinagreta de limón y aceite de oliva.
+🥬 Lechuga
+🍄 Setas
+🫘 Alubias
+🧅 Cebolla
+🌽 Maíz
+🫐 Arándanos
+🫒 Aceitunas
+🍍 Piña
+🥭 Mango
+🥕 Zanahoria
+🍅 Tomate
+🥒 Pepino
+🥑 Aguacate
+🥓 Bacon
+🥚 huevo
+🧀 Queso
+Emoji Terra
 */
 
+const estilo = "%c";
 
-const estilo = "%c"
+const alubias = "🫘 Alubias";
 
-const estiloTitulo = "color:blue; font-size: 18px";
+const aguacate = "🥑 Aguacate";
 
+const huevo = "🥚 huevo";
 
+const cebolla = "🧅 Cebolla";
+
+const tomate = "🍅 Tomate";
+
+const lechuga = "🥬 Lechuga";
+
+const estiloTitulo = "color: red; font-size: 18px; font-style: italic;";
 
 //Título
 
@@ -29,7 +40,7 @@ console.log(`${estilo}Ensalada de alubias con aguacate y huevo`, estiloTitulo);
 
 //Ingredientes
 
-console.log("Ingredientes: alubias, aguacate, huevo, cebolla, tomate, lechuga.");
+console.log(`Ingredientes: ${alubias}, ${aguacate}, ${huevo}, ${cebolla}, ${tomate}, ${lechuga}.`);
 
 //Preparación
 
@@ -85,8 +96,8 @@ console.log(`${estilo}Ensalada de mango y queso feta`, estiloTitulo);
 
 // Ingredientes
 
-console.log("- Ingredientes: mango en cubos, queso feta, lechuga, cebolla, pepino, aceitunas.");
+console.log("Ingredientes: mango en cubos, queso feta, lechuga, cebolla, pepino, aceitunas.");
 
 // Preparación
 
-console.log("- Preparación: mezclar el mango en cubos con el queso feta desmenuzado, lechuga troceada, cebolla en juliana, pepino en rodajas y aceitunas. Aliñar con una vinagreta de limón y aceite de oliva.");
+console.log("Preparación: mezclar el mango en cubos con el queso feta desmenuzado, lechuga troceada, cebolla en juliana, pepino en rodajas y aceitunas. Aliñar con una vinagreta de limón y aceite de oliva.");
