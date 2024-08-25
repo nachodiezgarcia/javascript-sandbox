5c927a642f5144ae2d196c675a6521caa61c72bd
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-25 13:46:57 +0200

    Primera receta

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
-const nombre = "Paco";
+/*
+Ensalada de alubias con aguacate y huevo
+Ingredientes: alubias, aguacate, huevo, cebolla, tomate, lechuga.
+Preparación: cocer las alubias y dejar enfriar. Mezclar con aguacate en cubos, huevo cocido en rodajas, cebolla en juliana, tomate en cubos y lechuga troceada. Aliñar con aceite de oliva y vinagre.
+Ensalada de setas y bacon
+Ingredientes: setas, bacon, lechuga, tomate, cebolla, queso.
+Preparación: saltear las setas y el bacon hasta que estén dorados. Mezclar con lechuga troceada, tomate en cubos, cebolla en juliana y queso rallado. Aliñar con aceite de oliva y vinagre.
+Ensalada de arándanos y pollo
+Ingredientes: arándanos, pollo cocido, lechuga, zanahoria, queso, nueces.
+Preparación: mezclar los arándanos con el pollo cocido en cubos, lechuga troceada, zanahoria rallada, queso rallado y nueces picadas. Aliñar con una vinagreta de miel y mostaza.
+Ensalada de maíz y piña
+Ingredientes: maíz, piña en cubos, lechuga, pepino, cebolla, aguacate.
+Preparación: mezclar el maíz con la piña en cubos, lechuga troceada, pepino en rodajas, cebolla en juliana y aguacate en cubos. Aliñar con una vinagreta de aceite de oliva y vinagre.
+Ensalada de mango y queso feta
+Ingredientes: mango en cubos, queso feta, lechuga, cebolla, pepino, aceitunas.
+Preparación: mezclar el mango en cubos con el queso feta desmenuzado, lechuga troceada, cebolla en juliana, pepino en rodajas y aceitunas. Aliñar con una vinagreta de limón y aceite de oliva.
+*/
 
-const apellido = "Gomez";
+//Titulo
 
-const nombreCompleto = `nombre: ${nombre} 
-(Otra linea) y el apellido es ${apellido}`;
+console.log("%cEnsalada de alubias con aguacate y huevo", "color:blue; font-size: 18px");
 
-console.log(nombreCompleto);
+//Ingredientes
 
-const ciudad = "Madrid";
+console.log("Ingredientes: alubias, aguacate, huevo, cebolla, tomate, lechuga.");
 
-const edad = 30;
+//Preparación
 
-const mensaje = `Hola mi nombre es ${nombre} tengo ${edad} años y vivo en ${ciudad}.`;
-
-console.log(mensaje);
+console.log("Preparación: cocer las alubias y dejar enfriar. Mezclar con aguacate en cubos, huevo cocido en rodajas, cebolla en juliana, tomate en cubos y lechuga troceada. Aliñar con aceite de oliva y vinagre.");
