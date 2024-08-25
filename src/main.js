4e9f3d14711bb806df97a68dc2948c10ab80d872
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-25 15:04:36 +0200

    Laboratorio 3 terminado

--- src/main.js
+++ src/main.js
@@ -16,7 +16,24 @@ const HardRock = "🤘 Hard Rock";
 
 const Clasica = "🎼 Clásica";
 
-const style = "color: green ; font-size: 18px ; font-style: bold";
+const style = "color: green ; font-size: 18px ; font-weight: bold";
 
+const PrimerGrupo = `%c${nombre} The Beatles, ${año} 1960, ${si}, genero: ${PopRock}`;
 
-const PrimerGrupo = `${nombre} The Beatles, ${año} 1960, ${si}, genero: ${PopRock}`
+const SegundoGrupo = `%c${nombre} Queen, ${año} 1970, ${no}, genero: ${Rock}`;
+
+const TercerGrupo = `%c${nombre} ACDC, ${año} 1973, ${si}, genero: ${HardRock}`;
+
+const CuartoGrupo = `%c${nombre2} Ludwig van Beethoven, ${año} 1770, ${no}, genero: ${Clasica}`;
+
+const QuintoGrupo = `%c${nombre} The Rolling Stones, ${año} 1962, ${si}, genero: ${Rock}`;
+
+console.log(PrimerGrupo, style);
+
+console.log(SegundoGrupo, style);
+
+console.log(TercerGrupo, style);
+
+console.log(CuartoGrupo, style);
+
+console.log(QuintoGrupo, style);
