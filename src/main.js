a8a7bc0433aa0d4cb0ac56b51645caf475d06ae4
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-25 14:45:36 +0200

    Primer grupo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-const nombre = "Nombre Grupo";
+const nombre = "Nombre del Grupo:";
 
-const style = "color: green ; font-size: 18px ; "
+const nombre2 = "Compositor:";
+
+const año = "Año de fundación:";
+
+const si = "Activo: true";
+
+const no = "Activo: false";
+
+const PopRock = "🎵 Pop Rock";
+
+const Rock = "🎸 Rock";
+
+const HardRock = "🤘 Hard Rock";
+
+const Clasica = "🎼 Clásica";
+
+const style = "color: green ; font-size: 18px ; font-style: bold";
+
+
+const PrimerGrupo = `${nombre} The Beatles, ${año} 1960, ${si}, genero: ${PopRock}`
